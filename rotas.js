2296efafc891bf49c04f99e437cb9c6b363b0b88
FabianoAmaral1984 <fab-amaral@hotmail.com>
2023-04-10 21:36:32 -0300

    Rotas adicionadas

--- rotas.js
+++ rotas.js
@@ -10,4 +10,14 @@ routes.get("/",(req,res)=>{
     res.status(200).json(lista)
 })
 
+routes.get("/:id",(req,res)=>{
+    res.status(200).json(lista[req.params.id-1])
+})
+
+routes.delete("/:id",(req,res)=>{
+    lista.splice(req.params.id-1,1)
+    res.status(200).redirect("/")
+})
+
+
 module.exports =  routes
